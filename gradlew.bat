fc8fea86c1541ddc6afcd32f0eb75a71e18a9cbc
chinotunero <133407505+chinotunero@users.noreply.github.com>
2025-11-15 02:39:23 +0100

    Fix formatting of DEFAULT_JVM_OPTS in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -4,7 +4,7 @@ if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+set DEFAULT_JVM_OPTS=-Xmx64m -Xms64m
 
 if exist "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" (
     set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
